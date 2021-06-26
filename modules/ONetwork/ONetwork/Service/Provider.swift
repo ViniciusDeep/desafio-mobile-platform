e9fbbc726de239ddac0fbe91a0320e42328fe97c
Vinicius Deep <viniciuscorreia11@gmail.com>
2021-06-26 14:41:33 -0300

    🛠  Update: network layer

---
+++ modules/ONetwork/ONetwork/Service/Provider.swift
@@ -0,0 +1,70 @@
+//
+//  Service.swift
+//  ONetwork
+//
+//  Created by Vinicius Mangueira Correa on 26/06/21.
+//
+
+import Foundation
+
+public enum ProviderMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+public struct Provider<T: Decodable> {
+    
+    let session: Sessionable
+    
+    public init(session: Sessionable = URLSession.shared) {
+        self.session = session
+    }
+    
+    public func request(router: ProviderEndpoint, withMethod method: ProviderMethod, params: [String : Any]?, completion: @escaping (Result<T, Error>) -> () ) {
+        method.request(session: self.session,router: router, params: params) { (result) in
+            switch result {
+            case .failure(let error):
+                completion(.failure(error))
+            case .success(let data):
+                do {
+                    let refund = try JSONDecoder().decode(T.self, from: data)
+                    DispatchQueue.main.async {completion(.success(refund))}
+                } catch {
+                    DispatchQueue.main.async {completion(.failure(error))}
+                }
+            }
+        }
+    }
+}
+
+extension ProviderMethod {
+    public func request(session: Sessionable,router: ProviderEndpoint, params: [String: Any]?, completion: @escaping (Result<Data, Error>)-> ()) {
+        guard let url = URL(string: router.endpoint) else {return}
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = rawValue
+        
+        do {
+        
+            if let dicParams = params {
+                let data = try JSONSerialization.data(withJSONObject: dicParams, options: .init())
+                urlRequest.httpBody = data
+                urlRequest.setValue("application/json", forHTTPHeaderField: "content-type")
+            }
+            
+           session.dataTask(with: urlRequest) { (data, _, err) in
+                if let error = err { completion(.failure(error))}
+                guard let data = data else {return print("Does not load data")}
+                completion(.success(data))
+            }.resume()
+        } catch {
+            completion(.failure(error))
+        }
+    }
+}
+
+public protocol ProviderEndpoint{
+    var endpoint: String{ get }
+}
+
