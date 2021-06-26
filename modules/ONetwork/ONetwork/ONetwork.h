8b4537f814ad32e7bf0be6384c3fae50ed2b0a40
Vinicius Deep <viniciuscorreia11@gmail.com>
2021-06-26 13:38:26 -0300

    👨🏽‍🎨 Create: modules ONetwork & OUIKit

---
+++ modules/ONetwork/ONetwork/ONetwork.h
@@ -0,0 +1,18 @@
+//
+//  ONetwork.h
+//  ONetwork
+//
+//  Created by Vinicius Mangueira Correa on 26/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ONetwork.
+FOUNDATION_EXPORT double ONetworkVersionNumber;
+
+//! Project version string for ONetwork.
+FOUNDATION_EXPORT const unsigned char ONetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ONetwork/PublicHeader.h>
+
+
---
+++ modules/OUIKit/OUIKit/OUIKit.h
@@ -0,0 +1,18 @@
+//
+//  OUIKit.h
+//  OUIKit
+//
+//  Created by Vinicius Mangueira Correa on 26/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OUIKit.
+FOUNDATION_EXPORT double OUIKitVersionNumber;
+
+//! Project version string for OUIKit.
+FOUNDATION_EXPORT const unsigned char OUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OUIKit/PublicHeader.h>
+
+
