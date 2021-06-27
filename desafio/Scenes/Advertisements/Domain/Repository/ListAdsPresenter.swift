f2d5ad02d09ae6bcab764d730bd297bd94ae166f
Vinicius Deep <viniciuscorreia11@gmail.com>
2021-06-27 08:33:02 -0300

    👨🏽‍🎨 A: controler at listAds

---
+++ desafio/Scenes/Advertisements/Domain/Repository/ListAdsPresenter.swift
@@ -0,0 +1,28 @@
+//
+//  AdsListPresenter.swift
+//  desafio
+//
+//  Created by Vinicius Mangueira Correa on 27/06/21.
+//
+
+import ONetwork
+
+protocol AdsListPresentationLogic: AnyObject {
+    var viewController: AdsListViewController? { get set }
+    func show(ads: ListAds)
+    func show(error: ProviderError)
+}
+
+final class AdsListPresenter {
+    weak var viewController: AdsListViewController?
+}
+
+extension AdsListPresenter: AdsListPresentationLogic {
+    func show(ads: ListAds) {
+        
+    }
+    
+    func show(error: ProviderError) {
+        
+    }
+}
